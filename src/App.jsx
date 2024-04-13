51d7cedb4fc486e1748192bfc1bce4c6b960613a
ahmad gaber <ahmadgaber540@gmail.com>
2024-04-13 23:10:05 +0200

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
 function App() {
-
   return (
-    <div className="text-red-500">App</div>
-  )
+    <BrowserRouter className="text-red-500">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
