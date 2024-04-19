8e938633117a8834680999220c21719527f0fbd6
ahmad gaber <55169135+ahmadhomyed@users.noreply.github.com>
2024-04-19 02:10:23 +0200

    new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
             </li>
           </Link>
           <Link to={"/about"}>
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className="hidden sm:inline trext-slate-700 hover:underline">
               About
             </li>
           </Link>
